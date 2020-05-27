07205b932ed82e0bd933bd253d6ebbf69c49c663
Ty Richadson <tyLrichardson63@gmail.com>
2020-05-26 20:48:23 -0500

    heroku fixes

--- src/components/SearchButton/SearchButton.js
+++ src/components/SearchButton/SearchButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import { Call } from '@material-ui/icons';
+import Call from '@material-ui/icons/Call';
 import Grid from '@material-ui/core/Grid';
 
 
